18aa2c4d5e1e0312afff5c7e55136a9d5e803262
Annie Zhang <annie7hang@gmail.com>
2017-02-23 17:42:15 -0500

    W2v postprocessing

--- fusiontester.py
+++ fusiontester.py
@@ -1,13 +1,14 @@
 import MovieQA
 from src.tfidf import TfIdf
-from src.htmloutput import HtmlOutput 
+from src.htmloutput import HtmlOutput
 import datetime
 import logging
 import math
 import nltk
 import numpy as np
-from w2vtester import W2VTester 
+from w2vtester import W2VTester
 from util import log_time_info
+
 
 class FusionTester(object):
     def __init__(self, extension='plt'):
@@ -20,16 +21,17 @@ class FusionTester(object):
 
     def normalize(self, mat):
         if mat.ndim == 1:
-            return mat/(np.linalg.norm(mat)+1e-6)
-        return mat/(np.linalg.norm(mat, axis=1, keepdims=True)+1e-6)
+            return mat / (np.linalg.norm(mat) + 1e-6)
+        return mat / (np.linalg.norm(mat, axis=1, keepdims=True) + 1e-6)
 
     def score_tfidf(self, q):
         # Sentence vectors for question, answers, plot
         question_vec = self.tfidf.getSentenceVector(q.imdb_key, q.question)
         answer_matrix = self.tfidf.getSentenceVectors(q.imdb_key, q.answers)
-        plot_matrix = self.tfidf.getSentenceVectors(q.imdb_key, self.story_raw[q.imdb_key])
+        plot_matrix = self.tfidf.getSentenceVectors(
+            q.imdb_key, self.story_raw[q.imdb_key])
 
-        qscore = plot_matrix.dot(question_vec).reshape(-1,1)
+        qscore = plot_matrix.dot(question_vec).reshape(-1, 1)
         ascore = plot_matrix.dot(answer_matrix.T)
         score = ascore + qscore
         return score
@@ -47,31 +49,32 @@ class FusionTester(object):
         # Start testing.
         nCorrect = 0.0
         nCorrectSame = 0.0
-        prediction_distribution = [0,0,0,0,0]
-        correct_distribution = [0,0,0,0,0]
+        prediction_distribution = [0, 0, 0, 0, 0]
+        correct_distribution = [0, 0, 0, 0, 0]
         nTried = len(self.qa)
 
         for q in self.qa:
             tfidf_scores = np.amax(self.score_tfidf(q), axis=0)
             w2v_scores = np.amax(self.w2vtester.score(q), axis=0)
-            scores = weight*self.normalize(tfidf_scores) + self.normalize(w2v_scores)
+            scores = weight * \
+                self.normalize(tfidf_scores) + self.normalize(w2v_scores)
             prediction = scores.argmax()
 
             if prediction == q.correct_index:
-                nCorrect+=1
-            prediction_distribution[prediction] +=1
-            correct_distribution[q.correct_index] +=1
+                nCorrect += 1
+            prediction_distribution[prediction] += 1
+            correct_distribution[q.correct_index] += 1
 
-
-        print("TFIDF Accuracy:", nCorrect/nTried)
-        print("Percentage Overlap in Correct:", nCorrectSame/nCorrect)
+        print("TFIDF Accuracy:", nCorrect / nTried)
+        print("Percentage Overlap in Correct:", nCorrectSame / nCorrect)
         print("Predicted Answers: ", prediction_distribution)
-        print("Correct Answers: ",correct_distribution)
-        return(nCorrect/nTried)
+        print("Correct Answers: ", correct_distribution)
+        return(nCorrect / nTried)
+
 
 if __name__ == "__main__":
     fusion_tester = FusionTester()
-    weights =  [0.001, 0.01, 0.1, 1, 10, 100, 1000]
+    weights = [0.001, 0.01, 0.1, 1, 10, 100, 1000]
     weights_2 = [2.5, 5, 7.5, 12, 15, 17, 20]
     weights_3 = [20, 35, 50, 80, 120, 300, 500]
     accuracies = []
--- w2vtester.py
+++ w2vtester.py
@@ -4,21 +4,23 @@ from w2v.word2vec import Word2Vec
 
 
 class W2VTester(object):
-    def __init__(self, story_raw, qa, extension='plt'):
+
+    def __init__(self, story_raw, qa, extension='plt', postprocess=True):
         self.qa = qa
-        self.w2v = Word2Vec(extension)
+        self.w2v = Word2Vec(extension, postprocess=postprocess)
 
         # Build each plot into a matrix of sentence embeddings.
         self.story_matrices = {}
         for imdb_key in story_raw:
-            self.story_matrices[imdb_key] = self.w2v.get_vectors_for_raw_text(story_raw[imdb_key])
+            self.story_matrices[imdb_key] = self.w2v.get_vectors_for_raw_text(story_raw[
+                                                                              imdb_key])
 
     def score(self, q):
-        ''' Make a prediction for the QA. 
+        ''' Make a prediction for the QA.
         '''
         # Process question and answers.
-        question = self.w2v.get_sentence_vector(q.question) 
-        answers = self.w2v.get_text_vectors(q.answers)  
+        question = self.w2v.get_sentence_vector(q.question)
+        answers = self.w2v.get_text_vectors(q.answers)
 
         # Calculate similarity
         qscore = self.story_matrices[q.imdb_key].dot(question).reshape(-1, 1)
@@ -30,16 +32,16 @@ class W2VTester(object):
         scores = self.score(q)
         # Prediction is (plot line#, answer#)
         prediction = np.unravel_index(scores.argmax(), scores.shape)
-        #print scores.shape
-        #prediction = scores.argmax()
-        #print prediction
-        #return prediction, "", scores[prediction]
+        # print scores.shape
+        # prediction = scores.argmax()
+        # print prediction
+        # return prediction, "", scores[prediction]
         return prediction[1], prediction[0], scores[prediction]
 
     def test(self):
         ''' Run w2v on the plots.
         '''
-        
+
         # Start testing.
         nCorrect = 0.0
         nTried = len(self.qa)
@@ -47,14 +49,19 @@ class W2VTester(object):
         for q in self.qa:
             prediction, reference, confidence = self.predict(q)
             if prediction == q.correct_index:
-                nCorrect+=1
+                nCorrect += 1
 
-        
-        print("Word2Vec Accuracy:", nCorrect/nTried)
+        print("Word2Vec Accuracy:", nCorrect / nTried)
 
 
 if __name__ == "__main__":
     mqa = MovieQA.DataLoader()
+    print "-----Training Data-----"
     story_raw, qa = mqa.get_story_qa_data('train', 'plot')
+    w2v_tester = W2VTester(story_raw, qa)
+    w2v_tester.test()
+
+    print "-----Validation Data-----"
+    story_raw, qa = mqa.get_story_qa_data('val', 'plot')
     w2v_tester = W2VTester(story_raw, qa)
     w2v_tester.test()
