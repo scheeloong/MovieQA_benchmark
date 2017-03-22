114ddd5b9211a7748039d2d1f14047d217fecbb1
scheeloong <scheeloong@gmail.com>
2017-03-22 02:48:32 -0400

    Done initial parsing for memn2n

--- main.py
+++ main.py
@@ -21,94 +21,113 @@ def runMemN2N(trainPlots, testQuestions):
 
 class MemN2N(object):
     def __init__(self, story, qa, extension='plt', postprocess=True):
+        '''
+        -------------------------------------------------------
+        b = self.batchSize = 9 => 32
+        V = vocabulary size = 20 => HUGE
+        z = numberOfSentence = 10
+        l = numberOfWordsInSentence = 6
+        d = memn2n word_embedding_size = 2 => anyNumber
+        -------------------------------------------------------
+        W  = (d, V) 
+        story data (9, 10, 6) = (b, z, l)
+        Embeddings A (20, 2) = (V, d)
+        Embedding Lookup A (9, 10, 6, 2) = (b, z, l, d)
+        Memory Matrix M (9, 10, 2) = (b, z, d)
+        Question Data (9, 1, 6) = (b, 1, l)
+        Control Signal U (9, 1, 2) = (b, 1, d)
+        Memory Selection (9, 10, 1) = (b, z, 1)
+        p (9, 10, 1) = (b, z, 1)
+        c_set (9, 2, 10) = (b, d, z)
+        o (9, 2) = (b, d, 1)
+        o_u_sum = (b, 1, d)
+        Correct Prediction (9, 1) = (b, 1)
+        -------------------------------------------------------
+        '''
         # w2v contains the embeddings to be trained
 
         # TODO: Temporary hard coded values below
-        self.embedDim = 128 # TODO: Hard coded as that's what we trained for w2vec
         self.memorySize = 100
         self.batchSize = 32
         self.learningRate = 0.01
         self.w2v = Word2Vec(extension, postprocess=postprocess)
         self.qa = qa # all the questions
+
+        # Parse the values
+        self.word2VecDim = 0  # initialize
+        self.embedDim = 0 #initialize
+        self.maxNumSentences = 0  # initialize
+        self.maxNumAnswers = 0 # initialize
+        # Embeddings for the story matrices
         self.storyMatrices= {} # storyMatrices indexed by movieKey
-
-        # TODO FOR PARSING
-        self.vocabularySize =  999999 
-        self.maxNumSentences = 999999
-        self.sentenceLength = 99999
-
-
         for movieKey in story:
             self.storyMatrices[movieKey] = self.w2v.get_vectors_for_raw_text(story[movieKey])
+            self.maxNumSentences = max(self.maxNumSentences, self.storyMatrices[movieKey].shape[0])
+        for movieKey in story:
+            self.word2VecDim = self.storyMatrices[movieKey].shape[1]
+            self.embedDim = self.word2VecDim # TODO: Confirm if its okay to set as the same
+            break
 
-        # TODO:
-        # self.buildGraphRunSess()
+        for currQA in self.qa:
+            question = self.w2v.get_sentence_vector(currQA.question)
+            answers = self.w2v.get_text_vectors(currQA.answers) #print "ori question"
+            self.maxNumAnswers = answers.shape[0]
+            break;
+        print "maxNumSentence", self.maxNumSentences
+        print "word2VecDim", self.word2VecDim
+        print "numAnswers", self.maxNumAnswers
+    
+        self.vocabularySize = 999999  # TODO: Confirm if need this
+        # self.sentenceLength = 99999 # TODO: Confirm if dont need this
+
+        self.buildGraphRunSess()
 
     def buildGraphRunSess(self):
         #Create tensorflow graph
         graph = tf.Graph()
 
         with graph.as_default(), tf.device('/cpu:0'):
+            # TODO: Figure out if the sizes below are right
             story_data = tf.placeholder(tf.float32, shape=[None, self.maxNumSentences, self.vocabularySize], name="storydata")
             question_data = tf.placeholder(tf.float32, shape=[None, 1, self.vocabularySize], name="questiondata")
             answer_data = tf.placeholder(tf.float32, shape=[None, 1, self.vocabularySize], name="answerdata") #1hot vector of answer
             '''
-            storyLookUp = tf.placeholder(tf.float32, shape=[None, 1, self.sentenceLength], name="abcd") #1hot vector of answer
-            questionLookUp = tf.placeholder(tf.float32, shape=[None, 1, self.sentenceLength], name="abc") #1hot vector of answer
-            '''
 
-            #Prediction weight matrix
-            W = tf.Variable(tf.truncated_normal([self.embedDim, self.vocabularySize], stddev=0.1)) # 5.1 of paper
-            W_biases = tf.Variable(tf.truncated_normal([self.vocabularySize], stddev=0.1))
-
-            #Initialize random embeddings
+            # Initialize random embeddings
             # Initialize as normal distribution with mean = 0 and std.deviation = 1 according to paper
             # To perform matrix multiplication on higher dimensions
             batchSizing= tf.shape(story_data)[0]
-
+            # TODO: Initialize as pre-trained word vector
+            Z = tf.Variable(tf.truncated_normal([self.vocabularySize, self.word2VecDim]))
+            T = tf.Variable(tf.truncated_normal([self.word2VecDim, self.embedDim]))
             # To encode temporal information on which sentence we are currently on
-            # TODO: Index in reverse order 
-            memoryA = tf.Variable(tf.truncated_normal([self.maxNumSentences, self.embedDim], stddev=0.05), name="VariableEmbeddingA", dtype=tf.float32)
-            memoryB = tf.Variable(tf.truncated_normal([self.maxNumSentences, self.embedDim], stddev=0.05), name="VariableEmbeddingB")
+            memoryA = tf.Variable(tf.truncated_normal([self.maxNumSentences, self.embedDim], stddev=0.05))
+            memoryB = tf.Variable(tf.truncated_normal([self.maxNumSentences, self.embedDim], stddev=0.05))
+                
+            # TODO: only multiply the embeddings for question, answer, and plots
+            # (v, d)
+            embeddings_A = tf.matmul(Z, T)
+            embeddings_B = tf.matmul(Z, T)
+            embeddings_C = tf.matmul(Z, T)
+            embeddings_F = tf.matmul(Z, T)
 
-            embeddings_A = tf.Variable(tf.truncated_normal([self.vocabularySize, self.embedDim], stddev=0.05), name="VariableEmbeddingA", dtype=tf.float32)
-            embeddings_B = tf.Variable(tf.truncated_normal([self.vocabularySize, self.embedDim], stddev=0.05), name="VariableEmbeddingB")
-            embeddings_C = tf.Variable(tf.truncated_normal([self.vocabularySize, self.embedDim], stddev=0.05), name="VariableEmbeddingC")
-            '''
-            -------------------------------------------------------
-            b = self.batchSize = 9
-            V = vocabulary size = 20
-            z = numberOfSentence = 10
-            l = numberOfWordsInSentence = 6
-            d = word_embedding_size = 2
-            -------------------------------------------------------
-            W  = (d, V) 
-            story data (9, 10, 6) = (b, z, l)
-            Embeddings A (20, 2) = (V, d)
-            Embedding Lookup A (9, 10, 6, 2) = (b, z, l, d)
-            Memory Matrix M (9, 10, 2) = (b, z, d)
-            Question Data (9, 1, 6) = (b, 1, l)
-            Control Signal U (9, 1, 2) = (b, 1, d)
-            Memory Selection (9, 10, 1) = (b, z, 1)
-            p (9, 10, 1) = (b, z, 1)
-            c_set (9, 2, 10) = (b, d, z)
-            o (9, 2) = (b, d, 1)
-            o_u_sum = (b, 1, d)
-            Correct Prediction (9, 1) = (b, 1)
-            -------------------------------------------------------
-            '''
+            # (b, z, v) * (v, d)  = (b, z, d)
+            answerG = tf.reshape(tf.matmul(tf.reshape(story_data, (batchSizing*self.maxNumSentences,self.vocabularySize)), embeddings_F), (batchSizing, self.maxNumSentences, self.embedDim))
+
             memory_matrix_m = tf.reshape(tf.matmul(tf.reshape(story_data, (batchSizing*self.maxNumSentences,self.vocabularySize)), embeddings_A), (batchSizing, self.maxNumSentences, self.embedDim))
-            haha = memory_matrix_m  # TODO REMOVE FROM DEBUGGING
+
             # Hidden layers for word encodings (sum words to get sentence representation)
             # This gets a sentence representation for each sentence in a paragraph
             # Gets a single sentence representation for that 1 question
             control_signal_u= tf.matmul(tf.reshape(question_data, (batchSizing, self.vocabularySize)), embeddings_B)
+
             # Get training control values
             c_set = tf.reshape(tf.matmul(tf.reshape(story_data, (batchSizing*self.maxNumSentences,self.vocabularySize)), embeddings_C), (batchSizing, self.maxNumSentences, self.embedDim))
 
             # Add temporal information
             memory_matrix_m = tf.add(memory_matrix_m, memoryA)
             c_set = tf.add(c_set, memoryB)
+
             # Use memory multplied with control to select a story
             # (b,z,d) * (b,d,1) = (b,z,1)
             memory_selection = tf.reshape(tf.matmul(memory_matrix_m, tf.reshape(control_signal_u, (batchSizing, self.embedDim, 1))), (batchSizing, self.maxNumSentences, 1))
@@ -118,10 +137,16 @@ class MemN2N(object):
             c_set = tf.transpose(c_set, (0, 2, 1))
             o = tf.reshape(tf.matmul(c_set, tf.reshape(p,(batchSizing, self.maxNumSentences, 1))),(batchSizing, self.embedDim))
             # Calculate the sum
+            # (b, 1, d)
             o_u_sum = tf.add(o, control_signal_u)
-            # predicted_answer_labels = tf.matmul(o_u_sum, W) + W_biases 
-            predicted_answer_labels = tf.matmul(o_u_sum, W)
-            predicted_answer_labels = tf.reshape(predicted_answer_labels, [-1, 1, self.vocabularySize])
+            # answerG => (b, z, d) , z = numberSentence => numberAnswerChoices in this case
+            # TODO: Modify to get: (b, d) * (b, d, z) => (b, z) => For every batch, there is an answer
+            predicted_answer_labels = tf.matmul(o_u_sum, tf.transpose(answerG, (0, 2, 1)))
+            # (b,1,d) * (d, V) = (b, V) => Choose a vocabulary as an answer
+
+            # No idea if the bottom line is still needed
+            # predicted_answer_labels = tf.reshape(predicted_answer_labels, [-1, 1, self.vocabularySize]) 
+            predicted_answer_labels = tf.reshape(predicted_answer_labels, [-1, 1, self.vocabularySize]) 
             y_predicted = predicted_answer_labels
             answer_data = tf.reshape(answer_data, [batchSizing, 1, self.vocabularySize])
             y_target = answer_data
@@ -134,15 +159,16 @@ class MemN2N(object):
             loss = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(logits = y_predicted, labels = y_target))
             #Optimizer
             optimizer = tf.train.AdagradOptimizer(learningRate).minimize(loss)
+            '''
 
+            # TODO: Uncomment once done fixing everything above
+            '''
             # Run session
             loss_values = []
-
             with tf.Session(graph=graph) as session:
                 #Initialize variables
                 tf.global_variables_initializer().run()
                 val_story = valX[:]
-                #val_qu = np.reshape(valq[:], (valq.shape[0],1, self.sentenceLength))
                 val_qu = np.reshape(valq[:], (valq.shape[0],1, self.vocabularySize))
                 val_a = np.reshape(vala[:], (vala.shape[0],1, self.vocabularySize))
                 feed_dictV = {story_data: val_story, question_data: val_qu, answer_data: val_a}
@@ -189,6 +215,7 @@ class MemN2N(object):
             loss_value_array = np.array(loss_values)
             pylab.plot(np.arange(0,epoch_size, 1),loss_values)
             pylab.show()  
+            '''
 
 
 def runTfIdf(trainPlots, testQuestions):
