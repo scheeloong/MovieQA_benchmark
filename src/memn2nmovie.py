c66cd976ea113c3b92d5240ca76cc91b947ff8f3
scheeloong <scheeloong@gmail.com>
2017-03-22 00:47:43 -0400

    Updated

--- src/memn2nmovie.py
+++ src/memn2nmovie.py
@@ -1,22 +1,8 @@
+# Ignored nice class design etc. due to time constraints
 # This file is End to End Memory Networks Applied to MovieQA
 
-def __init__(self, extension='plt', postprocess=False):
-    # Filenames & Constants
-    ID_MAP_FILE = "id_map_%s.pkl" % extension
-    EMBED_FILE = "embed_%s.npz" % extension
-    self.SYMBOLS_TO_REMOVE = '"#$%&()*+,/:;<=>@ [\]^_`{|}~-' + "'?!"
-    # Get word2vec embeddings
-    self.embeddings = []
-    with np.load(EMBED_FILE) as f:
-        self.embeddings = f['embed']
-        print('Embeddings loaded')
+# TODO: Copy paste from main.py to here
+class MemN2N(object):
+    def __init__(self):
+        w2vLoader = Word2vecLoader()
 
-        # Get the word to word-id mappings
-        self.id_map = self.load_obj(ID_MAP_FILE)
-
-        if postprocess:
-            self._postprocess()
-
-        def load_obj(self, name):
-        with open(name, 'rb') as f:
-        return pickle.load(f)
